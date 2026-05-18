58ba8f1e705070f9c825991ba1cf095f6bb784ab
martinezdiemar-afk <martinezdiemar@gmail.com>
2026-05-19 00:58:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,25 +2,28 @@ import streamlit as st
 import joblib
 import numpy as np
 
-
+# cargar modelo
 model = joblib.load("modelo.pkl")
 
-st.title("🎓 Predicción de Nota Final (G3)")
+st.title("🎓 Sistema de Predicción de Rendimiento Académico")
 
-st.write("Introduce los datos del estudiante:")
+st.write("Predicción de si el estudiante aprobará o suspenderá")
 
-studytime = st.number_input("Tiempo de estudio (1-4)", min_value=1, max_value=4)
-failures = st.number_input("Número de suspensos anteriores", min_value=0, max_value=10)
-absences = st.number_input("Faltas de asistencia", min_value=0, max_value=100)
+# inputs
+studytime = st.number_input("Tiempo de estudio (1-4)", 1, 4)
+failures = st.number_input("Suspensos anteriores", 0, 10)
+absences = st.number_input("Faltas", 0, 100)
 
-G1 = st.number_input("Nota G1", min_value=0, max_value=20)
-G2 = st.number_input("Nota G2", min_value=0, max_value=20)
+G1 = st.number_input("Nota G1", 0, 20)
+G2 = st.number_input("Nota G2", 0, 20)
 
+if st.button("Predecir resultado"):
 
-if st.button("Predecir G3"):
-    
     features = np.array([[studytime, failures, absences, G1, G2]])
-    
+
     pred = model.predict(features)
 
-    st.success(f"🎯 Nota final estimada (G3): {pred[0]:.2f}")
+    if pred[0] == 1:
+        st.success("🟢 APROBADO")
+    else:
+        st.error("🔴 SUSPENDIDO")
